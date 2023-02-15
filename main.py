ca52009c473f0d1f4e8865900cb34576ead5dfda
Me <park.pavel04@gmail.com>
2023-02-16 02:04:36 +0600

    Create Menu

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ def gef_input():
     string = input("Input String: ")
     pattern = input("Input Pattern: ")
     return string, pattern
+
+#Menu Of Command:
+
+def menu(string, pattern):
+    while True:
+        print("\n Choose Operation:")
+        print("1 ~ Find All")
+        print("2 ~ Search")
+        print("3 ~ Split")
+        print("4 ~ Substitute")
+        print("5 ~ Exit")
+
+    choose = input("Input Your Choise:")
